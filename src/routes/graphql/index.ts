2db0c854d8a77d3c68f2aeed219d402c16869220
NikFreeman <Classic76@tut.by>
2024-03-04 11:34:44 +0300

    feat: add rule

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,12 +1,14 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { Source, graphql } from 'graphql';
+import { Source, graphql, parse, validate } from 'graphql';
 
 import { schema } from './types/schema.js';
+import depthLimit from 'graphql-depth-limit';
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
   const { prisma } = fastify;
+  const DEPTH_LIMIT = 5;
 
   fastify.route({
     url: '/',
@@ -20,6 +22,13 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
     async handler(req) {
       const { query, variables } = req.body;
       const source = new Source(query);
+      const validationErrors = validate(schema, parse(source), [depthLimit(DEPTH_LIMIT)]);
+
+      if (validationErrors.length !== 0) {
+        return {
+          errors: validationErrors,
+        };
+      }
 
       return graphql({ schema, source, variableValues: variables, contextValue: prisma });
     },
